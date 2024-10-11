e1d10fa455ac34a3818924bcf41c970335e6d178
Mogador <jcaillaud17170@gmail.com>
2024-10-11 11:52:24 +0200

    Fix Refacto Sensor

--- src/device/sensor/abstract/AbstractAppSensorOrientation.ts
+++ src/device/sensor/abstract/AbstractAppSensorOrientation.ts
@@ -3,6 +3,11 @@ import { AbstractAppSensor } from "./AbstractAppSensor";
 export abstract class AbstractAppSensorOrientation extends AbstractAppSensor {
     protected sensor!: OrientationSensor;
 
+    set onReading(onReading: (data: number[]) => void) {
+        this.sensor.onreading = () =>
+            onReading(this.sensor.quaternion ? this.sensor.quaternion : []);
+    }
+
     start(): void {
         this.sensor.start();
     }
